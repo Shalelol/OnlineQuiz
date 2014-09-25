834ed446efe9c4615be692ca65a9b94fe2a49f77
Shale Kuzmanovski <Shale.lol@hotmail.com>
2014-09-25 17:44:07 +1000

    + Added models for the quiz database along with a DbContext + Added controller for quizzes (QuizAttempt and QuizAnswer are untested)

---
+++ ShaleCo.OnlineQuiz.Web/ShaleCo.OnlineQuiz.Web/Controllers/QuizController.cs
@@ -0,0 +1,58 @@
+﻿using ShaleCo.OnlineQuiz.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShaleCo.OnlineQuiz.Web.Controllers
+{
+    public class QuizController : Controller
+    {
+        private QuizDbContext _context = new QuizDbContext();
+
+        //
+        // GET: /Quiz/
+        public ActionResult Index()
+        {
+            if (false)
+            {
+                var quiz = new Quiz();
+                quiz.TeacherName = User.Identity.Name;
+
+                var question = new Question();
+                question.Text = "What is 2 + 2?";
+
+                var answer1 = new Answer();
+                answer1.Text = "5";
+
+                var answer2 = new Answer();
+                answer2.Text = "4";
+
+                var answer3 = new Answer();
+                answer3.Text = "1,000,000";
+
+                var answer4 = new Answer();
+                answer4.Text = "15";
+
+                question.Answers.Add(answer1);
+                question.Answers.Add(answer3);
+                question.Answers.Add(answer4);
+                question.CorrectAnswer = answer2;
+
+                quiz.Questions.Add(question);
+
+                _context.Quizzes.Add(quiz);
+
+                _context.SaveChanges();
+            }
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+	}
+}
---
+++ ShaleCo.OnlineQuiz.Web/ShaleCo.OnlineQuiz.Web/Models/QuizModels.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ShaleCo.OnlineQuiz.Web.Models
+{
+    public class QuizDbContext : DbContext
+    {
+        public QuizDbContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<QuizAttempt> QuizAttempts { get; set; }
+    }
+
+    [Table("Quizzes")]
+    public class Quiz
+    {
+        public Quiz()
+        {
+            this.Questions = new List<Question>();
+        }
+
+        [Key]
+        public int QuizID { get; set; }
+        public string TeacherName { get; set; }
+        public virtual ICollection<Question> Questions { get; set; }
+    }
+
+    [Table("Questions")]
+    public class Question
+    {
+        public Question()
+        {
+            this.Answers = new List<Answer>();
+        }
+
+        [Key]
+        public int QuestionID { get; set; }
+        public int AnswerID { get; set; }
+
+        [ForeignKey("AnswerID")]
+        public Answer CorrectAnswer { get; set; }
+        public string Text { get; set; }
+        public virtual ICollection<Answer> Answers { get; set; }
+    }
+
+    [Table("Answers")]
+    public class Answer
+    {
+        [Key]
+        public int AnswerID { get; set; }
+        public string Text { get; set; }
+    }
+
+    [Table("QuizAttempts")]
+    public class QuizAttempt
+    {
+        [Key]
+        public int QuizAttemptID { get; set; }
+        public string StudentName { get; set; }
+
+        public int QuizID { get; set; }
+
+        [ForeignKey("QuizID")]
+        public Quiz Quiz { get; set; }
+
+        public virtual ICollection<QuizAnswer> Answers { get; set; }
+    }
+
+    public class QuizAnswer
+    {
+        [Key]
+        public int QuizAnswerID { get; set; }
+        public int QuestionID { get; set; }
+        [ForeignKey("QuestionID")]
+        public Question Question { get; set; }
+        public int AnswerID { get; set; }
+        [ForeignKey("AnswerID")]
+        public Answer Answer { get; set; }
+    }
+}
